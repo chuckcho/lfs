8fef04416623182759ef5ddc0591fa1f12395b03
Chuck Cho <ccho@axon.com>
2018-12-20 15:05:07 -0800

    name changes

--- scripts/image/run-image.sh
+++ scripts/image/run-image.sh
@@ -5,9 +5,9 @@ echo "Start building bootable image.."
 pushd /tmp
 mkdir isolinux
 
-sh /tools/1.configure-syslinux.sh
-sh /tools/2.create-ramdisk.sh
-sh /tools/3.build-iso.sh
+sh /tools/postbuild-1.configure-syslinux.sh
+sh /tools/postbuild-2.create-ramdisk.sh
+sh /tools/postbuild-3.build-iso.sh
 
 rm -rf isolinux
 popd
